086e0345913da2b8072327c896a2d6c80a8e6f94
Kuan-You Lin <linstudyhard1997@gmail.com>
2022-06-24 14:17:07 -0400

    Add style to LockUnlockButton

--- src/containers/common/LockUnlockButton/LockUnlockButton.tsx
+++ src/containers/common/LockUnlockButton/LockUnlockButton.tsx
@@ -1,20 +1,37 @@
+import useTranslation from "next-translate/useTranslation";
 import React, { useContext } from "react";
 import { Icon } from "semantic-ui-react";
 import { CustomizedButton } from "^@components/common";
-import { AccountContext } from "^@contexts";
+import { AccountContext, CampaignInfoDetailContext } from "^@contexts";
+import { color } from "^@styles/global";
 
 export const LockUnlockButton = () => {
   const account = useContext(AccountContext);
+  const { campaignInfo } = useContext(CampaignInfoDetailContext);
+  const { t } = useTranslation("common");
+  if (!account || !campaignInfo) {
+    return null;
+  }
+  const { isLocked } = campaignInfo;
   return (
     <CustomizedButton
       content={
         <span>
-          <Icon name="lock" />
-          Hello
+          <Icon name={isLocked ? "unlock" : "lock"} />
+          {isLocked
+            ? t("containers.lockUnlockButton.unlock")
+            : t("containers.lockUnlockButton.lock")}
         </span>
       }
       size="medium"
+      disabled={account !== campaignInfo.owner}
       loading={false}
+      style={{
+        color: color["dark-grey"],
+        backgroundColor: color.white,
+        borderColor: color["dark-grey"],
+        borderStyle: "solid",
+      }}
     />
   );
 };
